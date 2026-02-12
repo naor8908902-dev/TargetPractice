08bea9d5db21452d4bd602fdce6931168ad62922
naor8908902-dev <naor8908902@gmail.com>
2026-02-12 10:51:26 +0200

    Add files via upload

---
+++ reg.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAozTFT0NroGxKEgvtzsZt9gIPfFYqleWg",
+    authDomain: "iot1-1d895.firebaseapp.com",
+    databaseURL: "https://iot1-1d895-default-rtdb.firebaseio.com",
+    projectId: "iot1-1d895",
+    storageBucket: "iot1-1d895.firebasestorage.app",
+    messagingSenderId: "440705339090",
+    appId: "1:440705339090:web:1bb3bdb73bb70c9453270b"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.getElementById('regForm')?.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('password').value;
+
+    createUserWithEmailAndPassword(auth, email, pass)
+        .then(() => {
+            alert("החשבון נוצר בהצלחה! מועבר לדף הכניסה.");
+            window.location.href = "login.html";
+        })
+        .catch((err) => {
+            alert("שגיאה ברישום: " + err.message);
+        });
+});
